c7693490486407aaa91a9e16fbb5b209035e2383
ChrisKnowsDev <chrisharley81@gmail.com>
2024-07-16 13:16:45 -0400

    initial project cards

---
+++ src/data/project-card-info.ts
@@ -0,0 +1,28 @@
+const projectCardInfo = [
+  {
+    title: 'Interesting Title of Project',
+    description:
+      'This project is so amazing, you really must check it out. No really, right now. Go ahead, click the link.',
+    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+  },
+  {
+    title: 'Wow, Super Amazing Project',
+    description:
+      'Wait till you see this. Go ahead, click the link and see this amazing case study of all the great UX work I did',
+    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+  },
+  {
+    title: 'Probably the Best Project Ever',
+    description:
+      'This case study is so great, probabalistically speaking, it is the best one ever. No kidding, see for yourself.',
+    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+  },
+  {
+    title: 'Title of Project 4',
+    description:
+      'The title speaks for itself. Since you saw my previous case studies, I bet you are excited to see this one too. Go ahead, click the link and see for yourself.',
+    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+  }
+]
+
+export default projectCardInfo
