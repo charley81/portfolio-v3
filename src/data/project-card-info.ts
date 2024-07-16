a695d599c39263e518c92f202bf14caea8246f04
ChrisKnowsDev <chrisharley81@gmail.com>
2024-07-16 13:40:51 -0400

    swap out project image png for svg

--- src/data/project-card-info.ts
+++ src/data/project-card-info.ts
@@ -3,25 +3,25 @@ const projectCardInfo = [
     title: 'Interesting Title of Project',
     description:
       'This project is so amazing, you really must check it out. No really, right now. Go ahead, click the link.',
-    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+    image: 'https://utfs.io/f/741f674c-5acf-40d9-b25c-0c3c23952226-fk993i.svg'
   },
   {
     title: 'Wow, Super Amazing Project',
     description:
       'Wait till you see this. Go ahead, click the link and see this amazing case study of all the great UX work I did',
-    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+    image: 'https://utfs.io/f/741f674c-5acf-40d9-b25c-0c3c23952226-fk993i.svg'
   },
   {
     title: 'Probably the Best Project Ever',
     description:
       'This case study is so great, probabalistically speaking, it is the best one ever. No kidding, see for yourself.',
-    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+    image: 'https://utfs.io/f/741f674c-5acf-40d9-b25c-0c3c23952226-fk993i.svg'
   },
   {
     title: 'Title of Project 4',
     description:
       'The title speaks for itself. Since you saw my previous case studies, I bet you are excited to see this one too. Go ahead, click the link and see for yourself.',
-    image: 'https://utfs.io/f/47495818-f9fb-4cc5-8a0c-1a28640e58e3-ld5whj.png'
+    image: 'https://utfs.io/f/741f674c-5acf-40d9-b25c-0c3c23952226-fk993i.svg'
   }
 ]
 
