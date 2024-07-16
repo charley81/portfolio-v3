aad15d46b6c131d18bc867f537a7801c69437f8d
ChrisKnowsDev <chrisharley81@gmail.com>
2024-07-16 17:01:44 -0400

    complete styling for footer

---
+++ src/data/footer-social-links.ts
@@ -0,0 +1,20 @@
+const footerSocialLinks = [
+  {
+    name: 'Dribbble',
+    url: 'https://dribbble.com/'
+  },
+  {
+    name: 'Behance',
+    url: 'https://www.behance.net/'
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/charley81'
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/charley81/'
+  }
+]
+
+export default footerSocialLinks
