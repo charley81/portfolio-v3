e0aa0c0a15af923a6a1dc16863c5b9cd7d7a3f4a
ChrisKnowsDev <chrisharley81@gmail.com>
2024-07-17 16:38:02 -0400

    add about page info and styling for mobile

---
+++ src/data/experience.ts
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from 'uuid'
+
+const experienceData = [
+  {
+    id: uuidv4(),
+    company: 'Progressive Leasing',
+    position: 'Associate Software Engineer',
+    startDate: 'Jan 2023',
+    endDate: 'Mar 2024'
+  },
+  {
+    id: uuidv4(),
+    company: 'ChrisHarley.io',
+    position: 'UX Engineer',
+    startDate: 'May 2018',
+    endDate: 'Jan 2023'
+  },
+  {
+    id: uuidv4(),
+    company: 'Luxottica',
+    position: 'Distribution Supervisor',
+    startDate: 'Jul 2017',
+    endDate: 'Mar 2018'
+  },
+  {
+    id: uuidv4(),
+    company: 'Phillips Van Heusen',
+    position: 'Operations Assistant',
+    startDate: 'Jan 2016',
+    endDate: 'Jul 2017'
+  },
+  {
+    id: uuidv4(),
+    company: 'U.S. Army',
+    position: 'Division Operations, and Training Manager',
+    startDate: 'Jun 2005',
+    endDate: 'Oct 2015'
+  }
+]
+
+export default experienceData
