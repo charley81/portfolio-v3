0d2cbf27a88381d8144ee5b57fdf8345f4f0c3b8
ChrisKnowsDev <chrisharley81@gmail.com>
2024-10-19 16:03:57 -0400

    changed index styling to accomodate one project-card

--- src/data/project-card-info.ts
+++ src/data/project-card-info.ts
@@ -1,9 +1,8 @@
 const projectCardInfo = [
   {
     title: 'Personalized Latin Meals, Just a Tap Away',
-    description:
-      'UX Designer with a background in web development, combining technical expertise with a passion for creating user-centered digital experiences, now seeking to contribute to a dynamic team',
-    image: 'https://utfs.io/f/741f674c-5acf-40d9-b25c-0c3c23952226-fk993i.svg'
+    description: 'Seamless Ordering for Latin Food Lovers',
+    image: 'https://utfs.io/f/itB1v1cNMItpqiuZjjSNCMkEUt4xWFwiXRLj9T2I5v1AdBzn'
   }
 ]
 
