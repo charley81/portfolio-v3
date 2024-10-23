56f95503481f4337932df923e1aa7bc088f0304c
ChrisKnowsDev <chrisharley81@gmail.com>
2024-10-23 14:40:11 -0400

    overview, problem statement, and user text complete

---
+++ src/components/case-studies/mi-amors/index.ts
@@ -0,0 +1,7 @@
+import Hero from './hero.astro'
+import Overview from './overview.astro'
+import ProblemStatement from './problem-statement.astro'
+import Summary from './summary.astro'
+import Users from './users.astro'
+
+export { Hero, Overview, ProblemStatement, Summary, Users }
