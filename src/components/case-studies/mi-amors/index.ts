4692d56a39db25052e93bcba7590de24da6fd2c7
ChrisKnowsDev <chrisharley81@gmail.com>
2024-10-24 16:26:46 -0400

    empathize section complete

--- src/components/case-studies/mi-amors/index.ts
+++ src/components/case-studies/mi-amors/index.ts
@@ -4,5 +4,28 @@ import ProblemStatement from './problem-statement.astro'
 import Summary from './summary.astro'
 import Users from './users.astro'
 import Role from './role.astro'
+import Constraints from './constraints.astro'
+import Define from './define.astro'
+import DesignSystem from './design-system.astro'
+import Empathize from './empathize.astro'
+import HighFidelity from './high-fidelity.astro'
+import Ideate from './ideate.astro'
+import Prototype from './prototype.astro'
+import Testing from './testing.astro'
 
-export { Hero, Overview, ProblemStatement, Summary, Users, Role }
+export {
+  Hero,
+  Overview,
+  ProblemStatement,
+  Summary,
+  Users,
+  Role,
+  Constraints,
+  Define,
+  DesignSystem,
+  Empathize,
+  HighFidelity,
+  Ideate,
+  Prototype,
+  Testing
+}
