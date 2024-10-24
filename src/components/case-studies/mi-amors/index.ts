0280afd7a5093c6383ad6847a1990fea0ad203d2
ChrisKnowsDev <chrisharley81@gmail.com>
2024-10-24 12:34:57 -0400

    role section complete

--- src/components/case-studies/mi-amors/index.ts
+++ src/components/case-studies/mi-amors/index.ts
@@ -3,5 +3,6 @@ import Overview from './overview.astro'
 import ProblemStatement from './problem-statement.astro'
 import Summary from './summary.astro'
 import Users from './users.astro'
+import Role from './role.astro'
 
-export { Hero, Overview, ProblemStatement, Summary, Users }
+export { Hero, Overview, ProblemStatement, Summary, Users, Role }
