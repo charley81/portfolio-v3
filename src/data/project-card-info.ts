21cda87743210b3c9d7716939cb20cab01741e39
ChrisKnowsDev <chrisharley81@gmail.com>
2024-11-12 12:27:41 -0500

    updated homepage project card iamges

--- src/data/project-card-info.ts
+++ src/data/project-card-info.ts
@@ -2,12 +2,13 @@ const projectCardInfo = [
   {
     title: 'Personalized Latin Meals, Just a Tap Away',
     description: 'Seamless Ordering for Latin Food Lovers',
-    image: 'https://utfs.io/f/itB1v1cNMItpa6anRLitmCB73ZTb5paD6qOQus1gN8xzUhFd',
+    image: 'https://utfs.io/f/itB1v1cNMItpxlIaBZhVqmPhEYJ24Ca15rSd9WUgswBnu8TF',
     link: 'case-studies/mi-amors'
   },
   {
-    title: 'Chop Suey Books, Brand Site',
-    description: 'We got the books and the brand',
+    title: 'Excellent shopping experience, with a local touch',
+    description:
+      'community spirit with a streamlined shopping experience to engage',
     image: 'https://utfs.io/f/itB1v1cNMItpdjwVGd9Ws4ZD6bRcq8mvV1i7uQaKTxXtnCAM',
     link: 'case-studies/chop-suey'
   }
