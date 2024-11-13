acd8b4e37ada24c32c7f2a64bbb290e649ce60fa
ChrisKnowsDev <chrisharley81@gmail.com>
2024-11-13 11:47:29 -0500

    added sections up through users

--- src/components/case-studies/chop-suey/index.ts
+++ src/components/case-studies/chop-suey/index.ts
@@ -1,5 +1,9 @@
 import Hero from './hero.astro'
 import Summary from './summary.astro'
 import About from './about.astro'
+import Overview from './overview.astro'
+import ProblemStatement from './problem-statement.astro'
+import Users from './users.astro'
+import Role from './role.astro'
 
-export { Hero, Summary, About }
+export { Hero, Summary, About, Overview, ProblemStatement, Users, Role }
