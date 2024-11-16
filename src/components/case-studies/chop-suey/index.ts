4719a2b14f8ff819fa8be5e37fe8c4e5ae8cf47f
ChrisKnowsDev <chrisharley81@gmail.com>
2024-11-16 10:15:03 -0500

    added define section

--- src/components/case-studies/chop-suey/index.ts
+++ src/components/case-studies/chop-suey/index.ts
@@ -5,5 +5,23 @@ import Overview from './overview.astro'
 import ProblemStatement from './problem-statement.astro'
 import Users from './users.astro'
 import Role from './role.astro'
+import Constraints from './constraints.astro'
+import Empathize from './empathize.astro'
+import Define from './define.astro'
+import Ideate from './ideate.astro'
+import HighFidelity from './high-fidelity.astro'
 
-export { Hero, Summary, About, Overview, ProblemStatement, Users, Role }
+export {
+  Hero,
+  Summary,
+  About,
+  Overview,
+  ProblemStatement,
+  Users,
+  Role,
+  Constraints,
+  Empathize,
+  Define,
+  Ideate,
+  HighFidelity
+}
