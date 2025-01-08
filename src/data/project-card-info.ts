88af0ea3fb9374387381049b95912598425be299
ChrisKnowsDev <chrisharley81@gmail.com>
2025-01-08 14:45:48 -0500

    completed sbr card on index page

--- src/data/project-card-info.ts
+++ src/data/project-card-info.ts
@@ -12,6 +12,13 @@ const projectCardInfo = [
       "An engaging shopping experience that reflects Chop Suey Books' unique identity, community spirit, and curated charm.",
     image: 'https://utfs.io/f/itB1v1cNMItpdjwVGd9Ws4ZD6bRcq8mvV1i7uQaKTxXtnCAM',
     link: 'case-studies/chop-suey'
+  },
+  {
+    title: ' A UX Solution for the Drum & Bass Community',
+    description:
+      'A platform that enhances live streaming, fan engagement, and monetization for drum & bass DJs and fans',
+    image: 'https://utfs.io/f/itB1v1cNMItpC23HMGTTZM5VuYQeDgwsni4H3jSdJCyqbOch',
+    link: 'case-studies/sbr'
   }
 ]
 
