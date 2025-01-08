1031313f194cc170a6da86acd60f9b93a0904ba4
ChrisKnowsDev <chrisharley81@gmail.com>
2025-01-08 17:41:47 -0500

    added sections for overview, problem statement, role, constrains, and emphathize

--- src/components/case-studies/sbr/index.ts
+++ src/components/case-studies/sbr/index.ts
@@ -1,30 +1,27 @@
 import Hero from './hero.astro'
-// import Overview from './overview.astro'
-// import ProblemStatement from './problem-statement.astro'
-// import Summary from './summary.astro'
-// import Users from './users.astro'
-// import Role from './role.astro'
-// import Constraints from './constraints.astro'
+import Overview from './overview.astro'
+import Summary from './summary.astro'
+import ProblemStatement from './problem-statement.astro'
+import Role from './role.astro'
+import Constraints from './constraints.astro'
+import Empathize from './empathize.astro'
 // import Define from './define.astro'
-// import DesignSystem from './design-system.astro'
-// import Empathize from './empathize.astro'
-// import HighFidelity from './high-fidelity.astro'
 // import Ideate from './ideate.astro'
 // import Prototype from './prototype.astro'
 // import Testing from './testing.astro'
+// import HighFidelity from './high-fidelity.astro'
 // import Sketches from './sketches.astro'
 
 export {
-  Hero
-  // Overview,
-  // ProblemStatement,
-  // Summary,
-  // Users,
-  // Role,
-  // Constraints,
+  Hero,
+  Overview,
+  Summary,
+  ProblemStatement,
+  Role,
+  Constraints,
+  Empathize
   // Define,
   // DesignSystem,
-  // Empathize,
   // HighFidelity,
   // Ideate,
   // Prototype,
