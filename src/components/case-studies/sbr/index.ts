226e855d02e6d9a9e4ddb08ea5ed26f300cc1f37
ChrisKnowsDev <chrisharley81@gmail.com>
2025-01-08 13:22:48 -0500

    hero section complete

---
+++ src/components/case-studies/sbr/index.ts
@@ -0,0 +1,33 @@
+import Hero from './hero.astro'
+// import Overview from './overview.astro'
+// import ProblemStatement from './problem-statement.astro'
+// import Summary from './summary.astro'
+// import Users from './users.astro'
+// import Role from './role.astro'
+// import Constraints from './constraints.astro'
+// import Define from './define.astro'
+// import DesignSystem from './design-system.astro'
+// import Empathize from './empathize.astro'
+// import HighFidelity from './high-fidelity.astro'
+// import Ideate from './ideate.astro'
+// import Prototype from './prototype.astro'
+// import Testing from './testing.astro'
+// import Sketches from './sketches.astro'
+
+export {
+  Hero
+  // Overview,
+  // ProblemStatement,
+  // Summary,
+  // Users,
+  // Role,
+  // Constraints,
+  // Define,
+  // DesignSystem,
+  // Empathize,
+  // HighFidelity,
+  // Ideate,
+  // Prototype,
+  // Testing,
+  // Sketches
+}
