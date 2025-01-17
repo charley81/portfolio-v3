e364ddd3c8eeebea09d393a0d9eed289187cd0f2
ChrisKnowsDev <chrisharley81@gmail.com>
2025-01-17 16:49:48 -0500

    mi-amor case-study v2 mobile version complete

---
+++ src/components/shared/index.ts
@@ -0,0 +1,5 @@
+import CaseStudyHero from './case-study-hero.astro'
+import Overview from './overview.astro'
+import CaseStudySection from './case-study-section.astro'
+
+export { CaseStudyHero, Overview, CaseStudySection }
